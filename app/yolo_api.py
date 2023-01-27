33275533f3a4225d2fd272a78dab0fdfcd842eca
Jonas <jonas.haslauer@gmail.com>
2023-01-27 21:16:33 +0100

    Updated counter

--- app/yolo_api.py
+++ app/yolo_api.py
@@ -9,11 +9,12 @@ from cvu.detector.yolov5 import Yolov5 as Yolov5Onnx
 import threading
 import glob
 from pathlib import Path
+from multiprocessing import Value
 
 app = Flask(__name__)
 
 last_request_time = time.time()
-request_count = 0
+request_count = Value('i', 0)
 process_start_time = str(time.time())
 
 # load model
@@ -53,9 +54,9 @@ def detect_image(image, filename='static/0.jpg'):
 
 @app.route('/', methods=['GET', 'POST'])
 def process_request():
-    global request_count
+    with request_count.get_lock():
+        request_count.value = request_count.value + 1
 
-    request_count = request_count + 1
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
@@ -78,15 +79,16 @@ def process_request():
 @app.route('/metrics')
 def get_request_count():
     global last_request_time
-    global request_count
     global process_start_time
 
     current_time = time.time()
     request_time_difference = time.time() - last_request_time
     last_request_time = current_time
 
-    return_value = request_count / request_time_difference
-    request_count = 0
+    with request_count.get_lock():
+        return_value = request_count.value / request_time_difference
+        request_count.value = 0
+    
     metrics_string = '# TYPE requests_per_s gauge\nrequests_per_s ' + str(return_value) + '\n\n'
 
     # get current unix timestamp
